c8bf5960211185e789b83c26ef74b0bb65226b2d
Boopathi <Boopathi>
2021-02-10 19:04:07 +0530

    tested

--- src/main/java/pages/JSFB_DashBoard_Page.java
+++ src/main/java/pages/JSFB_DashBoard_Page.java
@@ -12,6 +12,45 @@ public class JSFB_DashBoard_Page extends ProjectWrapp{
 		this.test = test;
 	}
 
+	public JSFB_DashBoard_Page clickOverviewButton() throws InterruptedException{
+		locateFrameById("canvas");
+	     clickByXpathExplict(prop.getProperty("click.overview.button.xpath"));
+
+	 
+		return this;
+	}
+	
+	
+	public JSFB_DashBoard_Page verifyStatement(String accno,String Month) throws InterruptedException{
+clickByXpathExplict(".//a[text()='"+accno+"']");
+clickByXpathExplict(prop.getProperty("click.monthdropdown.xpath"));
+clickByXpathExplict(".//div[contains(text(),'"+Month+"')]");
+clickByXpathExplict(prop.getProperty("clickdetailsStmt.xpath"));
+
+
+
+
+
+		return this;
+	}
+
+
+
+
+	
+	
+	public JSFB_DashBoard_Page verifyaccSummary() throws InterruptedException{
+	
+	 VerifyElementPresent(prop.getProperty("click.accSummary.xpath"),"Account summary has been displaying","Account summary has not been displaying");
+		return this;
+		}
+		  
+	
+	
+	
+	
+	
+		
 	
 	
 	
