1faa4546086a2f875f6045298503f17dd4f9b4ce
Boopathi <Boopathi>
2021-02-11 11:05:06 +0530

    tesed

---
+++ src/main/java/testcases/Tc_02_ViewStatement.java
@@ -0,0 +1,45 @@
+package testcases;
+import java.io.IOException;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import com.relevantcodes.extentreports.ExtentTest;
+
+import pages.JSFB_LoginPage;
+import wrappers.ProjectWrapp;
+public class Tc_02_ViewStatement  extends ProjectWrapp {
+	@BeforeClass(groups={"common"})
+	public void setDatag() {
+		testCaseName="TC11";
+		testDescription="Login to Application";
+		browserName="Chrome";
+		dataSheetName="JSFBTestData";
+		category="Regression";
+		authors="Boopathi";
+		testKeyword="TC11";
+		LogoutStatus=true;	
+		usertype="CIBUser";
+	}
+
+	
+	@Test(groups={"sanity"},dataProvider="fetch")
+	public void checkAccSummary(String casename,String Username,String Password,String captcha,String otp,String accno,String month,String param3,String param4,String param5,String param6,String param7,String param8,String param9,String param10,String para11,String param12,String param13,String param14,String param15,String param16) throws Exception{
+	
+		String accnum=regvalue(accno);
+		try{	
+	new JSFB_LoginPage(driver, test)
+	.loginApplication(Username, Password, captcha)
+	.clickOverviewButton()
+	.verifyStatement(accnum,month);
+	
+	
+	
+	
+	logout(false);
+	}
+catch (Exception e) {
+	logout(false);
+}	
+}}
