4badf59f83931289b0469de04fab14374415e65d
User <User@DESKTOP-RV3G2MV>
2021-02-11 11:19:46 +0530

    ok

---
+++ src/main/java/pages/JSFB_ServicePage.java
@@ -0,0 +1,42 @@
+package pages;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.relevantcodes.extentreports.ExtentTest;
+
+import wrappers.ProjectWrapp;
+
+public class JSFB_ServicePage extends ProjectWrapp{
+	public  JSFB_ServicePage(RemoteWebDriver driver, ExtentTest test){
+		this.driver = driver;
+		this.test = test;
+	}
+	
+	
+	public JSFB_ServicePage clickUpdateEmail() throws InterruptedException{
+		
+	     clickByXpathExplict(prop.getProperty("click.update.email.xpath"));
+
+		return this;
+	}
+	
+	public JSFB_ServicePage FillEmailForm(String Email) throws InterruptedException{
+     
+enterByXpathExplict(prop.getProperty("enter.newemail.xpath"),Email);
+enterByXpathExplict(prop.getProperty("enter.confiememail.xpath"),Email);
+clickByXpathExplict(prop.getProperty("click.submit.xpath"));
+		return this;
+	}
+	public JSFB_ServicePage SubmitOTP(String OTP) throws InterruptedException{
+	     
+		enterByXpathExplict(prop.getProperty("enter.otp.xpath"),OTP);
+	
+		clickByXpathExplict(prop.getProperty("click.otp.submit.xpath"));
+				return this;
+			}
+	 
+ }
+ 	
+	
+	
+
