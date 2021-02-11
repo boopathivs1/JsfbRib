32889f6f5664e773e247ac6d8cd4627445bfa256
User <User@DESKTOP-RV3G2MV>
2021-02-11 10:49:39 +0530

    ok

---
+++ src/main/java/testcases/Tc_06_Update_Email.java
@@ -0,0 +1,45 @@
+package testcases;
+import java.io.IOException;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import com.relevantcodes.extentreports.ExtentTest;
+
+import pages.JSFB_LoginPage;
+import wrappers.ProjectWrapp;
+public class Tc_06_Update_Email  extends ProjectWrapp {
+	@BeforeClass(groups={"common"})
+	public void setDatag() {
+		testCaseName="TC06";
+		testDescription="Update Email";
+		browserName="Chrome";
+		dataSheetName="JSFBTestData";
+		category="Regression";
+		authors="Sreejith";
+		testKeyword="TC06";
+		LogoutStatus=true;	
+		usertype="CIBUser";
+
+	}
+	@Test(groups={"sanity"},dataProvider="fetch")
+	public void DEpositOverview(String casename,String Username,String Password,String captcha,String OTP,String Email,String param2,String param3,String param4,String param5,String param6,String param7,String param8,String param9,String param10,String para11,String param12,String param13,String param14,String param15,String param16) throws Exception{
+	try{	
+	new JSFB_LoginPage(driver, test)
+	.loginApplication(Username, Password, captcha)
+	.clickServices()
+	.clickUpdateEmail()
+	.FillEmailForm(Email)
+	.SubmitOTP(OTP);
+	logout(false);
+	}
+catch (Exception e) {
+	logout(false);
+}	
+
+	}
+	
+	
+	
+	}
