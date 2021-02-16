158ea46d541aa9c51b5dcc64bfc306b39e960c4c
Boopathi <Boopathi>
2021-02-16 12:27:52 +0530

    testwde

--- src/main/java/pages/Jana_recurring_Deposit.java
+++ src/main/java/pages/Jana_recurring_Deposit.java
@@ -13,7 +13,43 @@ public class Jana_recurring_Deposit  extends ProjectWrapp{
 	}
 
 
-	
+	public Jana_recurring_Deposit fillJanaRecurring(String sourceaccno,String amount,String daywise) throws InterruptedException{
+		
+		dropdownSelectionbyindex(1,"4534010000188902");
+		
+		
+		enterByXpathExplict(".//input[@id='installmentAmount']","1000");
+				//deposit frequency
+		
+		dropdownSelectionbyindex(2,"Monthly");
+		
+		dropdownSelectionbyindex(3,"1 Years");
+		
+
+		dropdownSelectionbyindex(4,"1 Months");
+		dropdownSelectionbyindex(5,"4534010000188392");
+		
+		clickByXpathExplict(".//input[@id='confirmBeneDtls']");
+				
+				
+
+
+
+
+				
+
+
+
+
+
+				
+
+				
+		
+		
+		
+		return this;
+	}
 	
 	
 	
