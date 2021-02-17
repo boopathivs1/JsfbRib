e4f171df8fab81e6256931fef3dc9cc874d1d810
Boopathi <Boopathi>
2021-02-17 19:03:46 +0530

    testtgd

---
+++ src/main/java/testcases/Tc_14_Otherbank_Transfer_NEFT.java
@@ -0,0 +1,55 @@
+package testcases;
+import java.io.IOException;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import com.relevantcodes.extentreports.ExtentTest;
+
+import pages.JSFB_LoginPage;
+import wrappers.ProjectWrapp;
+public class Tc_14_Otherbank_Transfer_NEFT  extends ProjectWrapp {
+	@BeforeClass(groups={"common"})
+	public void setDatag() {
+		testCaseName="TC14a";
+		testDescription="Otherbank_Transfer-NEFT";
+		browserName="Chrome";
+		dataSheetName="JSFBTestData";
+		category="Regression";
+		authors="Boopathi";
+		testKeyword="TC14a";
+		LogoutStatus=true;	
+		usertype="CIBUser";
+
+	}
+	@Test(groups={"sanity"},dataProvider="fetch")
+	public void DEpositOverview(String casename,String Username,String Password,String captcha,String accno,String name,String amount,String remarks,String otp,String fromacc,String amountRange,String param6,String param8,String param9,String param10,String para11,String param12,String param13,String param14,String param15,String param16) throws Exception{
+		try{	
+
+	new JSFB_LoginPage(driver, test)
+	.loginApplication(Username, Password, captcha)
+	.locatingFrames()
+	.clickfundtrans()
+	.clickInitiatetrans()
+	.clickOtherPayeeTab()
+	.submitOtherbankAccForm( accno,name,amount, remarks,"NEFT")
+	.otpVerify(otp)
+	.clickOkTransfer()
+	.clickTransferHistory()
+	.filterTransHistory(fromacc, amountRange)
+	.verifyHistoryAmount(amount)
+	.clickAccountMenu()
+	.verifyBalanceDeduction(fromacc);
+	logout(false);
+	}
+catch (Exception e) {
+	logout(false);
+	throw new Exception();
+}	
+
+	}
+	
+	
+	
+	}
